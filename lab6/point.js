449c828ba62abccfd7b27918aeccc870ab24075e
wmkyGIS <78177773+wmkyGIS@users.noreply.github.com>
2021-03-29 22:39:55 -0500

    point.js

---
+++ lab6/point.js
@@ -0,0 +1,9 @@
+var point = {
+"type": "FeatureCollection",
+"name": "PointLNK",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "stroke": null, "stroke-width": null, "stroke-opacity": null, "fill": null, "fill-opacity": null, "name": "Lincoln Municipal Airport (LNK)", "address": "2400 W. Adams St.", "marker-color": "#ffee00", "marker-size": "medium", "marker-symbol": "airport", "relevance": "historical marker", "description": "Facility constructed in 1974 ", "Name": null, "relevance 1": null, "url": null, "relevance 2": null, "source": null, "source url": null, "description 1": null, "source 1a": null, "source 1b": null, "location": null }, "geometry": { "type": "Point", "coordinates": [ -96.75494, 40.846624 ] } },
+{ "type": "Feature", "properties": { "stroke": null, "stroke-width": null, "stroke-opacity": null, "fill": null, "fill-opacity": null, "name": "Pester Airport", "address": null, "marker-color": "#000000", "marker-size": "medium", "marker-symbol": "airport", "relevance": null, "description": "Dr. Charles E. Carothers, dentist and aerial acrobat, crashed his plan here in November 1985. He later died in early 1986of the severe injuries he suffered from the crash.", "Name": null, "relevance 1": null, "url": null, "relevance 2": null, "source": "Carothers' obituary from Lincoln Journal Star, Friday, 03 January 1986, p. 7.", "source url": null, "description 1": null, "source 1a": null, "source 1b": null, "location": null }, "geometry": { "type": "Point", "coordinates": [ -96.571927070617676, 40.825890669478376 ] } }
+]
+}
