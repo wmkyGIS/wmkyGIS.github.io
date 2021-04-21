b7241a4d27e928ab427094508ec820b12ac679ea
wmkyGIS <78177773+wmkyGIS@users.noreply.github.com>
2021-04-20 19:40:09 -0500

    point.js

---
+++ FinalProject/point.js
@@ -0,0 +1,316 @@
+var point = [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#fff700",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Lincoln Municipal Airport",
+        "description": "– Tract chosen in 1928; construction completed in 1929; dedication in 1930 – Modern facility constructed in 1974",
+        "address": "2400 W Adams St, Lincoln, NE 68524",
+        "startDate": 1928,
+        "endDate": "Present",
+        "photo": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.75556182861328,
+          40.85066372436894
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-size": "medium",
+        "marker-symbol": "land-use",
+        "marker-color": "#46ff00",
+        "name": "Huskerville",
+        "description": "– University of Nebraska housing project for student veterans of World War II; included single men, and married men plus their families. (“They are Moving In,” The Nebraska State Journal, Lincoln, Nebraska, 15 Sept. 1946, p. 3)  – William F. And Robert W. Kurth of Lexington; James F. Erwin of Omaha – An FPHA project at the army airfield (LJS, 03 Oct. 1946) – Max Swan, housing manager of Huskerville (formerly of FHPA, then affiliated with the University): “The University of Nebraska has a much better housing situation than most universities…with its steam heat, hot running water and hard wood floors.” (LJS, 03 Oct. 1946) – In 1946, according to the Lincoln Journal Star, there were about 700 applicants wanting to live in Huskerville’s 253 family units. (LJS, 03 Oct. 1946) – 70 singles students were already living in their quarters, despite the capacity being 250 persons. (LJS, 03 Oct. 1946) – Day nursery for working mothers (LJS, 03 Oct. 1946) – Weekly bus tickets for those residents of Huskerville that need to go to campus for class (LJS, 03 Oct. 1946) – In 1952, residents of Huskerville were perplexed and frightened by a string of paralyzations and illness within their community. Polio was spreading in Huskerville. By the end of the year, two Huskerville children perished and 18 residents were paralyzed. It was not until 1956 that researchers from the University of Nebraska determined that the Huskerville polio outbreak was not spread from person-to-person. Instead, the cause was back-siphonage from improper toilet repairs. It was around the same time in the mid-1950s that Huskerville’s population steadily declined. (Breanna Fanta, “1952 Huskerville Polio Outbreak,” History Nebraska)  – Lincoln Air Force Base was fully de-activated by 1968 when the Air Force negotiated the breaking of the 99-year contract with the City of Lincoln. The Lincoln Airport Authority took control of the base in 1970. In 1971, the Arnold Heights housing area was open to the public. ",
+        "address": "",
+        "startDate": 1946,
+        "endDate": "Present"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.79349899291992,
+          40.8603691199945
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#46ff00",
+        "marker-size": "medium",
+        "marker-symbol": "land-use",
+        "name": "Bowling Lake",
+        "description": "—“Dug out by Air Force volunteers in the late 1950s, this lake served as a recreational area for airmen and their families that was closer to the base than similar venues in the city.” ",
+        "address": "NW 44th and Cuming St., Lincoln, NE, 68524",
+        "startDate": 1957,
+        "endDate": "Present",
+        "photo": "",
+        "source": "https://www.lincolnafb.org/BowlingLake.php"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.78467988967896,
+          40.86621114743755
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#fff700",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Lincoln Army Airfield/Lincoln Air Force Base",
+        "description": "– After heavy post-war demobilization, the Korean War sparked an urgent need to reactivate Lincoln Army Airfield and turn it into Lincoln Air Force Base and a Strategic Air Command bomber base; the civilian Lincoln airport remained operational during the Cold War – Branting, Lincoln Air Force Base Legacy Project – Branting, A Mighty Force for Peace, pp. 9-34 – On April 16, 2021, the governor of Nebraska temporarily “reopened” the Lincoln Air Force Base to house the 55th Wing while the outfit’s traditional home, Offutt Air Force Base, undergoes a multi-million-dollar runway renovation. It was the latest in a long relationship between the Lincoln Airport Authority, the Nebraska Air National Guard, and Offutt Air Force Base in the 21st century. (“Officials welcome Offutt planes to temporary home – ‘Lincoln Air Force Base’,” Lincoln Journal Star, 16 April 2021)",
+        "address": "Between present-day NW 48th and NW 47th Streets",
+        "startDate": 1952,
+        "endDate": "c.1963",
+        "photo": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.78662717342377,
+          40.85273313784536
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#46ff00",
+        "marker-size": "medium",
+        "marker-symbol": "land-use",
+        "name": "Former Nebraska State Fairgrounds",
+        "description": "– Biplanes were constructed and air shows conducted on these grounds throughout the 20th century – Former headquarters of Nebraska Aviation Corps, organized in Lincoln in July 1915 // \"Their early maneuvers included the experimental dropping of bombs from the flying airplanes.\" – Lincoln Standard Aircraft Company manufactured planes in the Industrial Arts Building here starting in 1919",
+        "startDate": 1901,
+        "endDate": 2009
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.6932487487793,
+          40.83524253023701
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#0000ff",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Harding, Zook, and Bahl",
+        "description": "– University of Nebraska students Brooks Harding, Abe Zook, and Errold Bahl established this airplane manufacturing and repair shop in July 1919. The company, headed by Lt. Errold Bahl, also advertised flying lessons in The Daily Nebraskan, the University’s newspaper. Bahl and his partners designed and built the Lark monoplane, and advertised it as an airplane for the “average man.” In May 1920, Bahl even took Executive Dean of the University of Nebraska on an aerial campus tour. – “University Students Will Build Airplanes,” The Daily Nebraskan, 02 February 1920. – “Dean Engberg Looks Down Upon University,” The Daily Nebraskan, 21 May 1920.",
+        "address": "107 N. 9th St., Lincoln, NE, 68508",
+        "startDate": 1919,
+        "endDate": "c.1921"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70893967151642,
+          40.813968585103105
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#0000ff",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Lincoln Airplane and Flying School",
+        "description": "",
+        "address": "2409 \"O\" St., Lincoln, NE, 68510",
+        "startDate": 1919,
+        "endDate": "c.1930s (Great Depression)"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.68633669614792,
+          40.813390034969004
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#0000ff",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Arrow Aircraft Factory",
+        "description": "",
+        "address": "4021 N. 56th St., Havelock, Lincoln, NE, 68504",
+        "startDate": 1929,
+        "endDate": "c.1940s",
+        "photo": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.6457414627075,
+          40.85361769094577
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#fff700",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Arrow Aircraft Airfield",
+        "description": "",
+        "address": "40.870000, -96.654444",
+        "startDate": "c.1928",
+        "endDate": "Unknown",
+        "photo": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.6537880897522,
+          40.87054365162727
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#fff700",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Union Airport",
+        "description": "320-acre airport closed in 1964; was home to Lincoln Airplane and Flying School by 1939.",
+        "address": "North of Fletcher Ave., between N 70th St. and US Hwy 77",
+        "startDate": 1930,
+        "endDate": 1964,
+        "photo": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.63368225097656,
+          40.881138431863725
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#fff700",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Pester Airport",
+        "description": "Dr. Charles E. Carothers, dentist and aerial acrobat, crashed his plan here in November 1985. He later died in early 1986of the severe injuries he suffered from the crash.",
+        "address": "",
+        "startDate": "Unknown",
+        "endDate": "Unknown",
+        "photo": "",
+        "source": "Carothers' obituary from Lincoln Journal Star, Friday, 03 January 1986, p. 7."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.57235622406006,
+          40.825955617503844
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#fff700",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Lincoln Aircraft airfield",
+        "description": "",
+        "address": "",
+        "startDate": 1924,
+        "endDate": "Unknown",
+        "photo": "",
+        "source": "Goeres, Wings Over Nebraska, p.49"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.69977188110352,
+          40.750345578879504
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#fff700",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "First Lincoln Standard Field before Lincoln Country Club (S. 20th St.)",
+        "description": "",
+        "address": "Intersection of S. 20th St. and High St. ",
+        "startDate": 1922,
+        "endDate": 1924
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.6895043849945,
+          40.78059830022966
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#fff700",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Ak-Sar-Ben Field",
+        "description": "\"Ak-Sar-Ben Field, whose hangars were located on the southeastern corner of these grounds, was used extensively during the early development of U.S. Postal Air Mail Service. Ray Benedict landed here with the first airmail from Chicago on May 15, 1920 and William DeWald piloted the return flight the same day. The Omaha-San Francisco leg of the transcontinental route was opened by Pilot Buck Heforn the following September. Daytime airmail service from New York to San Francisco began on September 8, 1920 and the first shipment from the east was flown into Omaha by James Murray. Although the early airmail flights took place during daylight hours, the feasibility of night flying was soon explored. On February 22, 1921, Jack Knight took off from the North Platte, Nebraska, airfield on the first night airmail flight in the United States. He landed here at Ak-Sar-Ben Field before continuing on to Chicago. The efforts of these pioneer aviators led to the inauguration, on July 1, 1924, of day-night airmail service from coast to coast. In September 1974 the Air Mail Pioneers of America met in Omaha to observe the 50th Anniversary of this significant event in aviation history.\"",
+        "address": "1110 South 67th St., Douglas County, Omaha, NE, 68182",
+        "startDate": 1920,
+        "source": "“Ak-Sar-Ben Field and the U.S. Air Mail,” Explore Nebraska History, accessed April 18, 2021, https://mynehistory.com/items/show/178.",
+        "endDate": "Unknown",
+        "photo": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.01590514183044,
+          41.24152529030956
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#fff700",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "name": "Offutt Air Force Base",
+        "description": "“Fort Crook became home to 61st Balloon Company…the airfield portion…was designated Offutt Field…in honor of 1st Lt. Jarvis J. Offutt [who] was killed while flying with the Royal Air Force in France and was Omaha’s first World War I air casualty.”",
+        "address": "55th Wing Public Affairs Division 109 Washington Square, Suite 21 Offutt AFB, NE 68113",
+        "startDate": 1924,
+        "endDate": "Present",
+        "photo": "",
+        "source": "Kooiman, Aviation Development in Nebraska (2000)"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -95.90927124023438,
+          41.11583136545
+        ]
+      }
+    }
+  ]
