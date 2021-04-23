2977bbb177fc2033e451dd17b97bc2a2be634cd3
wmkyGIS <78177773+wmkyGIS@users.noreply.github.com>
2021-04-23 08:55:14 -0500

    With firm endDates

--- FinalProject/groupedData.js
+++ FinalProject/groupedData.js
@@ -9,7 +9,7 @@ var data = [
         "description": "– Tract chosen in 1928; construction completed in 1929; dedication in 1930 – Modern facility constructed in 1974",
         "address": "2400 W Adams St, Lincoln, NE 68524",
         "startDate": 1928,
-        "endDate": "Present",
+        "endDate": 2021,
         "photo": ""
       },
       "geometry": {
@@ -30,7 +30,7 @@ var data = [
         "description": "– University of Nebraska housing project for student veterans of World War II; included single men, and married men plus their families. (“They are Moving In,” The Nebraska State Journal, Lincoln, Nebraska, 15 Sept. 1946, p. 3)  – William F. And Robert W. Kurth of Lexington; James F. Erwin of Omaha – An FPHA project at the army airfield (LJS, 03 Oct. 1946) – Max Swan, housing manager of Huskerville (formerly of FHPA, then affiliated with the University): “The University of Nebraska has a much better housing situation than most universities…with its steam heat, hot running water and hard wood floors.” (LJS, 03 Oct. 1946) – In 1946, according to the Lincoln Journal Star, there were about 700 applicants wanting to live in Huskerville’s 253 family units. (LJS, 03 Oct. 1946) – 70 singles students were already living in their quarters, despite the capacity being 250 persons. (LJS, 03 Oct. 1946) – Day nursery for working mothers (LJS, 03 Oct. 1946) – Weekly bus tickets for those residents of Huskerville that need to go to campus for class (LJS, 03 Oct. 1946) – In 1952, residents of Huskerville were perplexed and frightened by a string of paralyzations and illness within their community. Polio was spreading in Huskerville. By the end of the year, two Huskerville children perished and 18 residents were paralyzed. It was not until 1956 that researchers from the University of Nebraska determined that the Huskerville polio outbreak was not spread from person-to-person. Instead, the cause was back-siphonage from improper toilet repairs. It was around the same time in the mid-1950s that Huskerville’s population steadily declined. (Breanna Fanta, “1952 Huskerville Polio Outbreak,” History Nebraska)  – Lincoln Air Force Base was fully de-activated by 1968 when the Air Force negotiated the breaking of the 99-year contract with the City of Lincoln. The Lincoln Airport Authority took control of the base in 1970. In 1971, the Arnold Heights housing area was open to the public. ",
         "address": "",
         "startDate": 1946,
-        "endDate": "Present"
+        "endDate": 2021
       },
       "geometry": {
         "type": "Point",
@@ -47,10 +47,10 @@ var data = [
         "marker-size": "medium",
         "marker-symbol": "land-use",
         "name": "Bowling Lake",
-        "description": "The northeast portion of Lincoln Air Force Base was transformed from a 'dump' into a spot of 'beauty' in the late 1950s. Air base officials billed the project as a Cold War necessity. While under construction, newspapers constantly reported Bowling Lake’s dual purpose: '[T]he recreation area is to provide on-base recreation and to aid in the retention of high skilled airmen, [according to base officials]…Brig. Gen. Perry M. Hoisington II, 818th Air Division Commander, said the areas are needed due to the SAC [Strategic Air Command] alert concept which requires the men be constantly ready in the case of aggression.' Upon its completion in 1958, Bowling Lake hosted a star-studded 'christening' ceremony, a four-day 'Aqua-Air Show' extravaganza, and a solemn ceremony where the statue of Capt. Russell Bowling was unveiled by his family. ",
+        "description": "The northeast portion of Lincoln Air Force Base was transformed from a “dump” into a spot of “beauty” in the late 1950s. Air base officials billed the project as a Cold War necessity. While under construction, newspapers constantly reported Bowling Lake’s dual purpose: “[T]he recreation area is to provide on-base recreation and to aid in the retention of high skilled airmen, base officials say…Brig. Gen. Perry M. Hoisington II, 818th Air Division Commander, said the areas are needed due to the SAC [Strategic Air Command] alert concept which requires the men be constantly ready in the case of aggression.” Upon its completion in 1958, Bowling Lake hosted a star-studded “christening” ceremony, a four-day “Aqua-Air Show” extravaganza, and a solemn ceremony where the statue of Capt. Russell Bowling was unveiled by his family. ",
         "address": "NW 44th and Cuming St., Lincoln, NE, 68524",
         "startDate": 1957,
-        "endDate": "Present",
+        "endDate": 2021,
         "photo": "",
         "source": "https://www.lincolnafb.org/BowlingLake.php; “Show Will Kickoff Dedication of Lake” Lincoln Sunday Journal and Star, 13 April 1958; “More or Less Personal,” Lincoln Evening Journal and Nebraska State Journal, 09 June 1958"
       },
@@ -72,7 +72,7 @@ var data = [
         "description": "– After heavy post-war demobilization, the Korean War sparked an urgent need to reactivate Lincoln Army Airfield and turn it into Lincoln Air Force Base and a Strategic Air Command bomber base; the civilian Lincoln airport remained operational during the Cold War – Branting, Lincoln Air Force Base Legacy Project – Branting, A Mighty Force for Peace, pp. 9-34 – On April 16, 2021, the governor of Nebraska temporarily “reopened” the Lincoln Air Force Base to house the 55th Wing while the outfit’s traditional home, Offutt Air Force Base, undergoes a multi-million-dollar runway renovation. It was the latest in a long relationship between the Lincoln Airport Authority, the Nebraska Air National Guard, and Offutt Air Force Base in the 21st century. (“Officials welcome Offutt planes to temporary home – ‘Lincoln Air Force Base’,” Lincoln Journal Star, 16 April 2021)",
         "address": "Between present-day NW 48th and NW 47th Streets",
         "startDate": 1952,
-        "endDate": "c.1963",
+        "endDate": 1966,
         "photo": ""
       },
       "geometry": {
@@ -113,7 +113,7 @@ var data = [
         "description": "– University of Nebraska students Brooks Harding, Abe Zook, and Errold Bahl established this airplane manufacturing and repair shop in July 1919. The company, headed by Lt. Errold Bahl, also advertised flying lessons in The Daily Nebraskan, the University’s newspaper. Bahl and his partners designed and built the Lark monoplane, and advertised it as an airplane for the “average man.” In May 1920, Bahl even took Executive Dean of the University of Nebraska on an aerial campus tour. – “University Students Will Build Airplanes,” The Daily Nebraskan, 02 February 1920. – “Dean Engberg Looks Down Upon University,” The Daily Nebraskan, 21 May 1920.",
         "address": "107 N. 9th St., Lincoln, NE, 68508",
         "startDate": 1919,
-        "endDate": "c.1921"
+        "endDate": 1921
       },
       "geometry": {
         "type": "Point",
@@ -133,7 +133,7 @@ var data = [
         "description": "03/31/1930: the Lincoln Airplane and Flying School was officially approved by the US Department of Commerce: Aeronautics Branch. The Lincoln Airplane and Flying School operated out of two locations: the recently constructed municipal airport housed the flight training program, while mechanics training took place at the company’s headquarters in downtown Lincoln (pictured below at 2409 “O” Street).  The school briefly provided flight instruction to army cadets in 1939, before the Army Air Corps ¬– deterred by a particularly harsh Nebraska winter – opted to move the training program to Lakeland, Florida, the following fall. The facility closed in 1945, but the mantle of flight instruction in Lincoln was quickly taken up by other aviation-minded entrepreneurs.",
         "address": "2409 \"O\" St., Lincoln, NE, 68510",
         "startDate": 1919,
-        "endDate": "c.1930s (Great Depression)"
+        "endDate": "1930-1939"
       },
       "geometry": {
         "type": "Point",
@@ -153,7 +153,7 @@ var data = [
         "description": "",
         "address": "4021 N. 56th St., Havelock, Lincoln, NE, 68504",
         "startDate": 1929,
-        "endDate": "c.1940s",
+        "endDate": "1940-1949",
         "photo": ""
       },
       "geometry": {
@@ -173,8 +173,8 @@ var data = [
         "name": "Arrow Aircraft Airfield",
         "description": "",
         "address": "40.870000, -96.654444",
-        "startDate": "c.1928",
-        "endDate": "Unknown",
+        "startDate": 1928,
+        "endDate": 1986,
         "photo": ""
       },
       "geometry": {
@@ -212,13 +212,13 @@ var data = [
         "marker-color": "#fff700",
         "marker-size": "medium",
         "marker-symbol": "airport",
-        "name": "Pester Airport",
+        "name": "Pester Airport / NE59",
         "description": "Dr. Charles E. Carothers, dentist and aerial acrobat, crashed his plan here in November 1985. He later died in early 1986of the severe injuries he suffered from the crash.",
         "address": "",
-        "startDate": "Unknown",
-        "endDate": "Unknown",
+        "startDate": 1964,
+        "endDate": 2021,
         "photo": "",
-        "source": "Carothers' obituary from Lincoln Journal Star, Friday, 03 January 1986, p. 7."
+        "source": "Carothers' obituary from Lincoln Journal Star, Friday, 03 January 1986, p. 7; https://www.airnav.com/airport/NE59"
       },
       "geometry": {
         "type": "Point",
@@ -238,7 +238,7 @@ var data = [
         "description": "",
         "address": "",
         "startDate": 1924,
-        "endDate": "Unknown",
+        "endDate": "1930-1939",
         "photo": "",
         "source": "Goeres, Wings Over Nebraska, p.49"
       },
@@ -281,7 +281,7 @@ var data = [
         "address": "1110 South 67th St., Douglas County, Omaha, NE, 68182",
         "startDate": 1920,
         "source": "“Ak-Sar-Ben Field and the U.S. Air Mail,” Explore Nebraska History, accessed April 18, 2021, https://mynehistory.com/items/show/178.",
-        "endDate": "Unknown",
+        "endDate": 1935,
         "photo": ""
       },
       "geometry": {
@@ -302,7 +302,7 @@ var data = [
         "description": "“Fort Crook became home to 61st Balloon Company…the airfield portion…was designated Offutt Field…in honor of 1st Lt. Jarvis J. Offutt [who] was killed while flying with the Royal Air Force in France and was Omaha’s first World War I air casualty.”",
         "address": "55th Wing Public Affairs Division 109 Washington Square, Suite 21 Offutt AFB, NE 68113",
         "startDate": 1924,
-        "endDate": "Present",
+        "endDate": 2021,
         "photo": "",
         "source": "Kooiman, Aviation Development in Nebraska (2000)"
       },
@@ -315,4 +315,3 @@ var data = [
       }
     }
   ]
-
